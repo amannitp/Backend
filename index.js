508eca1442046283f24a77a51d525d0ba94ac36e
amannitp <amannitp272@gmail.com>
2023-10-16 10:30:27 +0530

    create a server

---
+++ index.js
@@ -0,0 +1,12 @@
+// server creation 
+
+const http =require('http')
+//console.log(http)
+
+ const server=http.createServer(()=>{
+
+})
+
+server.listen(5000,()=>{
+    console.log('Server is working')
+})
